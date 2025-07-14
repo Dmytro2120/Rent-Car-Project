713ec7d9b362e4c810120365503a6fabc3219ed9
Dmytro <godovdmytrro@gmail.com>
2025-07-14 13:29:29 +0200

    new tabel button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,93 @@
 import { useState } from "react";
 import "./App.css";
 import Button from "./componets/Button/Button";
+import Wechat from "./componets/Wechat";
 
 function App() {
   return (
     <>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="minimal"></Button>
-      <Button variant=""></Button>
+      <div className="container">
+        <table className="batton-table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Lable</th>
+              <th>Icon-Left</th>
+              <th>Icon</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Enabled</td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button
+                  icon={<Wechat />}
+                  variant="primary"
+                  size="small"
+                ></Button>{" "}
+              </td>
+              <td>
+                <Button
+                  icon={<Wechat />}
+                  variant="primary"
+                  size="small"
+                ></Button>
+              </td>
+            </tr>
+            <tr>
+              <td>Hower</td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+            </tr>
+            <tr>
+              <td>Pressed</td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+            </tr>
+            <tr>
+              <td>Focuse</td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button variant="primary" size="small"></Button>
+              </td>
+            </tr>
+            <tr>
+              <td>Disable</td>
+              <td>
+                <Button disabled variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button disabled variant="primary" size="small"></Button>
+              </td>
+              <td>
+                <Button disabled variant="primary" size="small"></Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
--- src/componets/Button/Button.jsx
+++ src/componets/Button/Button.jsx
@@ -2,23 +2,38 @@ import "./Button.css";
 import clsx from "clsx";
 
 const variants = ["primary", "secondary", "minimal"];
+const size = ["small", "medium", "large"];
+const defaultChildren = "Lable";
 
-export const Button = ({ variant, onClick }) => {
+export const Button = ({
+  variant,
+  onClick,
+  size,
+  disabled,
+  children = defaultChildren,
+  icon,
+}) => {
   // console.log(variant, !variant);
-  console.log(variant, variants.includes(variant));
+  // console.log(variant, variants.includes(variant));
   if (!variant || !variants.includes(variant))
     throw new Error("The 'variant' prop is required.");
+  // console.log(onClick);
 
   return (
     <button
       className={clsx(
+        "button",
         variant === "primary" && "button-primary",
         variant === "secondary" && "button-secondary",
         variant === "minimal" && "button-minimal",
+        size === "small" && "button-size-small",
+        size === "medium" && "button-size-medium",
+        size === "large" && "button-size-large",
       )}
       onClick={onClick}
+      disabled={disabled}
     >
-      Label
+      {icon} {children}
     </button>
   );
 };
