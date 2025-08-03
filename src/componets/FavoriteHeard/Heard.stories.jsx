a8881749517cb6b2da296603975751b7c297dd11
Dmytro <godovdmytrro@gmail.com>
2025-08-03 12:50:13 +0200

    add heard svg

---
+++ src/componets/FavoriteHeard/Heard.stories.jsx
@@ -0,0 +1,12 @@
+import { fn } from "storybook/test";
+
+import { Heard } from "./Heard";
+
+export default {
+  title: "Components/Heard",
+  component: Heard,
+};
+
+export const Default = {
+  args: { value: false },
+};
