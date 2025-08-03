6d9afb667ee8aef1a7c20a9766faed7db63ec090
Dmytro <godovdmytrro@gmail.com>
2025-08-03 10:13:38 +0200

    add button storybook new

--- src/componets/Button/Button.stories.jsx
+++ src/componets/Button/Button.stories.jsx
@@ -1,13 +1,54 @@
-// import { Button } from "./Button";
+import { fn } from "storybook/test";
 
+import { Button } from "./Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
-  title: "test/Button",
-  componnent: <div>test</div>,
+  title: "Componets/Button",
+  component: Button,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "select" }, // Automatically inferred when 'options' is defined
+    },
+    variant: {
+      options: ["primary", "secondary", "minimal"],
+      control: { type: "select" },
+    },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
 };
 
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
-    label: "Button",
+    children: "test",
+    variant: "primary",
+  },
+};
+
+export const Secondary = {
+  args: {
+    variant: "primary",
+  },
+};
+
+export const Large = {
+  args: {
+    variant: "primary",
+  },
+};
+
+export const Small = {
+  args: {
+    variant: "primary",
   },
 };
